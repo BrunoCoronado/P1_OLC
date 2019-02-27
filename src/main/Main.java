c3adf93956f8c3b9fa3be750b1999ab88a2c2756
Bruno Coronado <bcoronado.morales@gmail.com>
2019-02-27 01:06:32 -0600

    Ventana Principal
    Creacion inicial de la ventana principal

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import sistema.ui.Principal;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		Principal principal = new Principal();
+		principal.setVisible(true);
 	}
 
 }
--- src/sistema/ui/Principal.java
+++ src/sistema/ui/Principal.java
@@ -1,12 +1,42 @@
 package sistema.ui;
 
-public class Principal {
-	
-	Principal(){
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Principal extends JFrame{
+	//declaracion de componenetes visuales
+	JMenuBar menuBar;
+	JMenu menu, subMenu;
+	JMenuItem menuItem;
+
+	public Principal(){
 		iniciarComponentes();
 	}
 	
 	private void iniciarComponentes() {
-		
+		//parametros de la ventana
+		this.setSize(800, 800);
+		//creacion del menu bar
+		menuBar = new JMenuBar();
+		menu = new JMenu("menu A");
+		menuItem = new JMenuItem("menu item A");
+		menu.add(menuItem);
+		menuItem = new JMenuItem("menu item B");
+		menu.add(menuItem);
+		menuItem = new JMenuItem("menu item C");
+		menu.add(menuItem);
+		menu.addSeparator();
+		subMenu = new JMenu("sub menu");
+		menuItem = new JMenuItem("menu item A");
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("menu item B");
+		subMenu.add(menuItem);
+		menuItem = new JMenuItem("menu item C");
+		subMenu.add(menuItem);
+		menu.add(subMenu);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
 	}
 }
